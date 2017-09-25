0f69b31edb00a4b09ebcbcba8c3de23abcdd684c
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 15:59:12 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 class Pokemon
 
-  attr_accessor :name, :type, :db, :id
+  attr_accessor :name, :type, :db, :id, :hp
 
-  def initialize(name = "", type = "", id = "", db = "")
+  def initialize(name = "", type = "", id = "", hp = nil, db = "")
     @name = name
     @type = type
     @db = db
+    @hp = hp
     @id = id
   end
 
@@ -19,8 +20,13 @@ class Pokemon
     pokemon_id = found_pokemon[0]
     pokemon_name = found_pokemon[1]
     pokemon_type = found_pokemon[2]
+    pokemon_hp = found_pokemon[3]
 
-    new_pokemon = Pokemon.new(pokemon_name, pokemon_type, pokemon_id, db)
+    new_pokemon = Pokemon.new(pokemon_name, pokemon_type, pokemon_id, pokemon_hp, db)
+  end
+
+  def fight(new_hitpoint_value, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hitpoint_value, self.id)
   end
 
 end
